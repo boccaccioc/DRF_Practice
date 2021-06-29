9853d643e6e2e8f3596e32fe3888a75edbd72fe2
Colin Boccaccio <cmb171@duke.edu>
2021-06-29 16:50:39 -0400

    downloaded django-heroku localy to fix heroku

--- config/urls.py
+++ config/urls.py
@@ -18,5 +18,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('apis/v1/', include('apis.urls')), # new
+    path('apis/v1/', include('apis.urls'))
 ]
